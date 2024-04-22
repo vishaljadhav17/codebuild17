e8aa4291374abb1e2cbaade13ee067ec454223bb
Vishal Jadhav <124550491+vishaljadhav17@users.noreply.github.com>
2024-04-23 01:23:27 +0530

    Create start_container.sh

---
+++ start_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull vishaljadhav17/simple-python-flask-app
+
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 vishaljadhav17/simple-python-flask-app
